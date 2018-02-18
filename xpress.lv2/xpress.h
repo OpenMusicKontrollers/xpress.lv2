85a95decc56fd4248fd524087cdccdfbdb5ca52e
Hanspeter Portner <dev@open-music-kontrollers.ch>
2018-02-18 10:05:42 +0100

    Make voice.source a random URID.

--- xpress.lv2/xpress.h
+++ xpress.lv2/xpress.h
@@ -23,6 +23,7 @@ extern "C" {
 #endif
 
 #include <stdlib.h>
+#include <time.h>
 
 #include <lv2/lv2plug.in/ns/lv2core/lv2.h>
 #include <lv2/lv2plug.in/ns/ext/urid/urid.h>
@@ -109,7 +110,7 @@ struct _xpress_iface_t {
 };
 
 struct _xpress_voice_t {
-	xpress_uuid_t source;
+	LV2_URID source;
 	xpress_uuid_t uuid;
 	bool alive;
 	void *target;
@@ -232,6 +233,32 @@ xpress_map(xpress_t *xpress);
 
 static const xpress_state_t xpress_vanilla;
 
+//tools.ietf.org/html/rfc4122 version 4
+static LV2_URID
+_xpress_urn_uuid(LV2_URID_Map *map)
+{
+	uint8_t bytes [0x10];
+
+	srand(time(NULL));
+
+	for(unsigned i=0x0; i<0x10; i++)
+		bytes[i] = rand() & 0xff;
+
+	bytes[6] = (bytes[6] & 0b00001111) | 0b01000000; // set four most significant bits of 7th byte to 0b0100
+	bytes[8] = (bytes[8] & 0b00111111) | 0b10000000; // set two most significant bits of 9th byte to 0b10
+
+	char uuid [46];
+	snprintf(uuid, sizeof(uuid),
+		"urn:uuid:%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+		bytes[0x0], bytes[0x1], bytes[0x2], bytes[0x3],
+		bytes[0x4], bytes[0x5],
+		bytes[0x6], bytes[0x7],
+		bytes[0x8], bytes[0x9],
+		bytes[0xa], bytes[0xb], bytes[0xc], bytes[0xd], bytes[0xe], bytes[0xf]);
+
+	return map->map(map->handle, uuid);
+}
+
 static inline void
 _xpress_qsort(xpress_voice_t *A, int n)
 {
@@ -293,7 +320,7 @@ _xpress_voice_get(xpress_t *xpress, xpress_uuid_t uuid)
 }
 
 static inline void *
-_xpress_voice_add(xpress_t *xpress, xpress_uuid_t source, xpress_uuid_t uuid, bool alive)
+_xpress_voice_add(xpress_t *xpress, LV2_URID source, xpress_uuid_t uuid, bool alive)
 {
 	if(xpress->nvoices >= xpress->max_nvoices)
 		return NULL; // failed
@@ -362,7 +389,7 @@ xpress_init(xpress_t *xpress, const size_t max_nvoices, LV2_URID_Map *map,
 			: NULL;
 	}
 
-	xpress->source = xpress_map(xpress);
+	xpress->source = _xpress_urn_uuid(map);
 
 	return 1;
 }
@@ -386,7 +413,7 @@ xpress_advance(xpress_t *xpress, LV2_Atom_Forge *forge, uint32_t frames,
 
 	if(obj->body.otype == xpress->urid.xpress_Token)
 	{
-		const LV2_Atom_Int *source = NULL;
+		const LV2_Atom_URID *source = NULL;
 		const LV2_Atom_Int *uuid = NULL;
 		const LV2_Atom_Int *zone = NULL;
 		const LV2_Atom_Float *pitch = NULL;
@@ -408,7 +435,7 @@ xpress_advance(xpress_t *xpress, LV2_Atom_Forge *forge, uint32_t frames,
 			xpress->urid.xpress_dTimbre, &dTimbre,
 			0);
 
-		if(  !source || (source->atom.type != forge->Int)
+		if(  !source || (source->atom.type != forge->URID)
 			|| !uuid || (uuid->atom.type != forge->Int) )
 		{
 			return 0;
@@ -462,7 +489,7 @@ xpress_advance(xpress_t *xpress, LV2_Atom_Forge *forge, uint32_t frames,
 	}
 	else if(obj->body.otype == xpress->urid.xpress_Alive)
 	{
-		const LV2_Atom_Int *source = NULL;
+		const LV2_Atom_URID *source = NULL;
 		const LV2_Atom_Tuple *body = NULL;
 
 		lv2_atom_object_get(obj,
@@ -470,7 +497,7 @@ xpress_advance(xpress_t *xpress, LV2_Atom_Forge *forge, uint32_t frames,
 			xpress->urid.xpress_body, &body,
 			0);
 
-		if(  !source || (source->atom.type != forge->Int) )
+		if(  !source || (source->atom.type != forge->URID) )
 			return 0;
 
 		if(body && (body->atom.type == forge->Tuple) ) // non-existent body is a valid empty body
@@ -504,7 +531,7 @@ xpress_advance(xpress_t *xpress, LV2_Atom_Forge *forge, uint32_t frames,
 
 		XPRESS_VOICE_FOREACH(xpress, voice)
 		{
-			if(  (voice->source == (xpress_uuid_t)source->body)
+			if(  (voice->source == source->body)
 				&& !voice->alive )
 			{
 				if( (xpress->event_mask & XPRESS_EVENT_DEL) && xpress->iface->del)
@@ -612,7 +639,7 @@ xpress_token(xpress_t *xpress, LV2_Atom_Forge *forge, uint32_t frames,
 		if(ref)
 			ref = lv2_atom_forge_key(forge, xpress->urid.xpress_source);
 		if(ref)
-			ref = lv2_atom_forge_int(forge, xpress->source);
+			ref = lv2_atom_forge_urid(forge, xpress->source);
 
 		if(ref)
 			ref = lv2_atom_forge_key(forge, xpress->urid.xpress_uuid);
@@ -676,7 +703,7 @@ xpress_alive(xpress_t *xpress, LV2_Atom_Forge *forge, uint32_t frames)
 		if(ref)
 			ref = lv2_atom_forge_key(forge, xpress->urid.xpress_source);
 		if(ref)
-			ref = lv2_atom_forge_int(forge, xpress->source);
+			ref = lv2_atom_forge_urid(forge, xpress->source);
 
 		if(ref)
 			ref = lv2_atom_forge_key(forge, xpress->urid.xpress_body);
