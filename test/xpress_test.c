b1e756bd51ee90f45c8f989c6c92f5c2f85c9e3b
Hanspeter Portner <dev@open-music-kontrollers.ch>
2019-03-21 08:54:45 +0100

    add unit test for voice allocation overflows.

--- test/xpress_test.c
+++ test/xpress_test.c
@@ -8,6 +8,7 @@
 typedef struct _targetI_t targetI_t;
 typedef struct _plughandle_t plughandle_t;
 typedef struct _urid_t urid_t;
+typedef void (*test_t)(xpress_t *xpressI);
 
 struct _targetI_t {
 	int64_t frames;
@@ -111,27 +112,88 @@ static xpress_map_t voice_map = {
 	.new_uuid = _new_uuid
 };
 
+static void
+_test_1(xpress_t *xpressI)
+{
+	xpress_uuid_t uuid = 0;
+	targetI_t *src = xpress_create(xpressI, &uuid);
+	assert(uuid != 0);
+	assert(src != NULL);
+
+	assert(xpress_get(xpressI, uuid) == src);
+	assert(xpress_free(xpressI, uuid) == 1);
+	assert(xpress_get(xpressI, uuid) == NULL);
+	assert(xpress_free(xpressI, uuid) == 0);
+}
+
+static void
+_test_2(xpress_t *xpressI)
+{
+	xpress_uuid_t uuids [MAX_NVOICES] = { 0 };
+
+	for(unsigned i = 0; i < MAX_NVOICES; i++)
+	{
+		xpress_uuid_t uuid = 0;
+		targetI_t *src = xpress_create(xpressI, &uuid);
+		assert(uuid != 0);
+		assert(src != NULL);
+
+		assert(xpress_get(xpressI, uuid) == src);
+
+		uuids[i] = uuid;
+	}
+
+	// test overflow
+	{
+		xpress_uuid_t uuid = 0;
+		targetI_t *src = xpress_create(xpressI, &uuid);
+		assert(uuid != 0);
+		assert(src == NULL);
+	}
+
+	// test uniqueness of uuids and srcs
+	for(unsigned i = 0; i < MAX_NVOICES; i++)
+	{
+		targetI_t *src0 = xpress_get(xpressI, uuids[i]);
+
+		for(unsigned j = 0; j < MAX_NVOICES; j++)
+		{
+			targetI_t *src1 = xpress_get(xpressI, uuids[j]);
+
+			if(i == j)
+			{
+				assert(uuids[i] == uuids[j]);
+				assert(src0 == src1);
+			}
+			else
+			{
+				assert(uuids[i] != uuids[j]);
+				assert(src0 != src1);
+			}
+		}
+	}
+}
+
+static const test_t tests [] = {
+	_test_1,
+	_test_2,
+	NULL
+};
+
 int
 main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
 {
 	static plughandle_t handle;
 
-	assert(xpress_init(&handle.xpressI, MAX_NVOICES, &map, &voice_map,
-			XPRESS_EVENT_ALL, &ifaceI, handle.targetI, &handle) == 1);
+	for(const test_t *test = tests; *test; test++)
+	{
+		assert(xpress_init(&handle.xpressI, MAX_NVOICES, &map, &voice_map,
+				XPRESS_EVENT_ALL, &ifaceI, handle.targetI, &handle) == 1);
 
-	//FIXME run tests here
-	
-	xpress_deinit(&handle.xpressI);
+		(*test)(&handle.xpressI);
 
-	xpress_uuid_t uuid = 0;
-	targetI_t *src = xpress_create(&handle.xpressI, &uuid);
-	assert(uuid != 0);
-	assert(src);
-
-	assert(xpress_get(&handle.xpressI, uuid) == src);
-	assert(xpress_free(&handle.xpressI, uuid) == 1);
-	assert(xpress_get(&handle.xpressI, uuid) == NULL);
-	assert(xpress_free(&handle.xpressI, uuid) == 0);
+		xpress_deinit(&handle.xpressI);
+	}
 
 	for(unsigned i=0; i<urid; i++)
 	{
